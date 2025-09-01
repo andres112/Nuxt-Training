b86c881d875be88e3b0380a163a665ad1861ed3a
Andres Felipe Dorado <andres.dorado90@gmail.com>
2025-09-01 16:41:52 +0200

    Refactor code structure for improved readability and maintainability

--- crash-course/app/pages/index.vue
+++ crash-course/app/pages/index.vue
@@ -2,7 +2,7 @@
 	<div>
 		<h1>Welcome to Nuxt 4!</h1>
 		<p>This is the default page.</p>
-		<NuxtLink to="/about">Go to About Page</NuxtLink>
+		<NuxtLink to="/about" class="btn-primary">Go to About Page</NuxtLink>
 	</div>
 </template>
 
--- crash-course/nuxt.config.ts
+++ crash-course/nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
 	modules: ["@nuxt/eslint", "@nuxt/test-utils"],
 	devtools: { enabled: true },
 	app: {
 		pageTransition: { name: "slide-up", mode: "out-in" },
+	},
+	vite: {
+		plugins: [tailwindcss()],
 	},
 	css: ["~/assets/styles/main.scss"],
 	compatibilityDate: "2025-07-15",
