f66788463dbccda148aa9efd2bad49057bd24778
Andres Felipe Dorado <andres.dorado90@gmail.com>
2025-09-02 13:19:08 +0200

    feat: enhance styling and structure by integrating Tailwind CSS, adding design tokens, and refining typography

--- app/components/welcome.vue
+++ app/components/welcome.vue
@@ -4,13 +4,13 @@
 
 <template>
 	<div class="Welcome flex flex-col items-center justify-center min-h-screen gap-4">
-		<h1 class="Welcome__title text-4xl font-bold underline">
+		<h1 class="Welcome__title">
 			Welcome to Nuxt 4!
 		</h1>
 		<p>This is the default page.</p>
 		<NuxtLink
 			to="/about"
-			class="Welcome__btn inline-block rounded px-4 py-2 bg-blue-600 text-white hover:bg-blue-700"
+			class="Welcome__btn btn-primary"
 		>
 			Go to About Page
 		</NuxtLink>
@@ -18,6 +18,4 @@
 </template>
 
 <style scoped>
-.Welcome {
-}
 </style>
--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,11 +1,16 @@
 <template>
-	<div>
-		About This Page
-		<NuxtLink
-			to="/"
-			style="display: block; margin-top: 1rem;"
-		>
-			<button>Go to Home Page</button>
+	<div class="AboutPage container flex flex-col items-center justify-center min-h-screen gap-4">
+		<h2>
+			AboutPage This Page
+		</h2>
+		<NuxtLink to="/" class="btn-primary">
+			Go to Home Page
 		</NuxtLink>
 	</div>
 </template>
+
+<style scoped>
+.AboutPage {
+	padding-block: 1rem;
+}
+</style>
